1dcac3fd5ba615beb442352e2a13ae6224f567e3
ZSMingNB <zhaoshuming114@gmail.com>
2020-07-17 16:27:25 +0800

    项目目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,28 @@
 <script>
-//import axios from 'axios'
-//import HelloWorld from './components/HelloWorld.vue'
+
 import jsonp from 'jsonp'
 
 export default {
   name: 'app',
-  // components: {
-  //   HelloWorld
-  // },
+  components: {
+
+  },
   data(){
     return {
-      age:30,
-      data:''
+   
     }
   },
   mounted(){
-    //let url="https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list"    
-    // axios.get(url).then(()=>{
-    // })
-    let url="/api/activity/servicetime"
-    jsonp(url,(err,res)=>{
-      let result = res;
-      this.data = result;
+    
     })
   }
 }
 </script>
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view>
+
+    </router-view>
   </div>
 </template>
 
