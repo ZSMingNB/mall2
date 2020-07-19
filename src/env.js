86774527d90201d92f94517e02d4d478d26f226a
ZSMingNB <zhaoshuming114@gmail.com>
2020-07-19 22:34:41 +0800

    接口环境设置1

---
+++ src/env.js
@@ -0,0 +1,20 @@
+let baseURL;
+//process的相关文档,获取nodejs当前进程的环境变量
+switch (process.env.NODE_ENV) {
+    case 'dev':
+        baseURL = 'http://dev-mall-pre.springboot.cn/api';
+        break;
+    case 'test':
+        baseURL = 'http://test-mall-pre.springboot.cn/api';
+        break;
+    case 'prod':
+        baseURL = 'http://mall-pre.springboot.cn/api';
+        break;
+    default:
+        baseURL = 'http://mall-pre.springboot.cn/api';
+        break;
+}
+
+export default{
+    baseURL
+}
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
+import env from './env'  //不加./认为是插件
 
 //根据前端的跨域方式做调整
 //axios.defaults.baseURL = 'http://www.baidu.com'; //后端与前端域名不一样的时候
@@ -12,6 +13,8 @@ import App from './App.vue'
 axios.defaults.baseURL = '/api'; 
 //超时时间
 axios.defaults.timeout = 8000;
+//根据环境变量获取不同的请求地址
+axios.defaults.baseURL = env.baseURL;
 //接口错误拦截
 axios.interceptors.response.use(function(response){
   //这个才是接口取到的值
